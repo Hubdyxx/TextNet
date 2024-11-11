3e428ef64fdcbb34897e79eeaa16557987427cd1
Hubdyxx <hubdyxx@gmail.com>
2024-11-11 18:07:51 +0100

    Initial Commit

---
+++ TextNet/TextNetApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextNetApp.swift
+//  TextNet
+//
+//  Created by Hubert Kotlarz on 11/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct TextNetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
